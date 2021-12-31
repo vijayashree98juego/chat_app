309f52d5806ace0d3e406b4cd0994a39f2c32ba2
vijayashree98juego <vijayashree@juegostudioz.com>
2021-12-31 13:02:16 +0530

    sign in UI and functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import SignIn from './SignIn.js';
+import ChatList from './ChatList.js';
+class App extends Component {
+  
+  
+  render() {
+    return (
+      <div>
+      <SignIn/>
+      
+{/* <BrowserRouter>
+           <Router>
+          <Route exact path="/" component={<SignIn/>} />
+          <Route  path="/chat" component={<ChatList/>} />
+          </Router>
+          </BrowserRouter>
+  */}
+</div>
+    
+    );
+  }
 }
+
+
 
 export default App;
---
+++ src/SignIn.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import './SignIn.css'
+import ChatList from './ChatList';
+
+class SignIn extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            username:'',
+            password:'',
+            type:2,
+            redirect:false,
+            access_token:'',
+            errors:'',
+        }
+     
+        this.onChangeUsername= this.onChangeUsername.bind(this);
+        this.onChangePassword=this.onChangePassword.bind(this);
+        this.onSubmitHandler = this.onSubmitHandler.bind(this);
+    }
+ 
+    
+    onChangeUsername(e){
+        let errors=e.target.value?{username:'',password:this.state.errors['password']}:{username:'username cannot be empty',password:this.state.errors['password']};
+        this.setState(
+            {
+                username:e.target.value,
+                errors:errors
+            }
+        )
+    }
+    onChangePassword(e){
+      let errors=e.target.value?{password:'',username:this.state.errors['username']}:{password:'password cannot be empty',username:this.state.errors['username']}
+        this.setState({
+            password:e.target.value,
+            errors:errors
+        })
+    }
+
+    onSubmitHandler(){     
+        let username=this.state.username;
+        let password=this.state.password;
+        let errors = {}
+      if(!username){
+        errors['username']='username cannot be empty!!!';
+      }
+      if(!password){
+          errors['password']='password cannot be empty!!!';
+      }
+ 
+    if(Object.entries(errors).length===0){
+        console.log("processing submit operation....")
+        const data = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body:"type="+this.state.type+"&email="+this.state.username+"&password="+this.state.password 
+        };
+       
+        fetch('https://api-us.juegogames.com/NOMOS-V3/users/sign_in',data)
+        .then(response => response.json())
+        .then(data => {
+            if(data.responseCode===200){
+                console.log("success");
+                console.log(data.responseData)
+            }else{
+                alert("Incorrect username or password")
+            }
+        });
+    }else{
+        this.setState({
+            errors:errors
+        })
+    }
+      
+    }
+
+   
+
+    render() {
+      
+        return (
+            <div >
+                <h2 className='container-header'>LOGIN PAGE</h2>              
+                <div className="container">
+            <div className="row">
+                <div className="col-10">
+                    <label for="username">Username:</label>
+                </div>
+                <div className="col-90">
+                    <input type="text" id="username" name="username" placeholder="Enter your username" onChange={this.onChangeUsername} required/>
+                    <span style={{ color: "red" }}>{this.state.errors["username"]}</span>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-10">
+                    <label for="passsword">Password:</label>
+                </div>
+                <div className="col-90">
+                    <input type="password" id="password" name="password" placeholder="Enter password" onChange={this.onChangePassword} required/>
+                    <span style={{ color: "red" }}>{this.state.errors["password"]}</span>
+                </div>
+            </div>
+            <div className="row">
+                <input type="submit" value="Sing In" onClick={this.onSubmitHandler} />
+            </div> 
+            </div>
+            </div>
+        );
+    }
+}
+
+
+export default SignIn;
